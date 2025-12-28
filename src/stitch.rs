b454ac7474e64ba5b106b3d8a72959a6b8266607
bourbonut <ben.bourbon06@gmail.com>
2025-12-28 18:15:04 +0100

    Reimplement `stitch.rs` with `HashMap<[i32; 2], Rc<RefCell<Fragment>>>`

--- src/stitch.rs
+++ src/stitch.rs
@@ -1,6 +1,6 @@
 use std::{
-    collections::{HashMap, hash_map::Values},
-    ops::Add,
+    cell::RefCell,
+    collections::{HashMap, HashSet, hash_map::Values},
     rc::Rc,
 };
 
@@ -12,46 +12,51 @@ pub fn stitch(topology: &TopoJSON, arcs: Vec<i32>) -> Vec<Vec<i32>> {
 
 #[derive(Debug, PartialEq, Clone)]
 struct Fragment {
-    start: Rc<Vec<i32>>,
-    end: Rc<Vec<i32>>,
-    arcs: Rc<Vec<i32>>,
+    start: [i32; 2],
+    end: [i32; 2],
+    arcs: Vec<i32>,
 }
 
 impl Fragment {
-    fn new(start: Vec<i32>, end: Vec<i32>, arcs: Vec<i32>) -> Self {
-        Self {
-            start: Rc::new(start),
-            end: Rc::new(end),
-            arcs: Rc::new(arcs),
-        }
+    fn new(start: [i32; 2], end: [i32; 2], arcs: Vec<i32>) -> Self {
+        Self { start, end, arcs }
     }
 
     fn push(&mut self, item: i32) {
-        Rc::get_mut(&mut self.arcs).unwrap().push(item);
+        self.arcs.push(item);
     }
 
     fn unshift(&mut self, value: i32) {
-        Rc::get_mut(&mut self.arcs).unwrap().insert(0, value);
+        self.arcs.insert(0, value);
     }
 }
 
-impl<'a, 'b> Add<&'b Fragment> for &'a Fragment {
-    type Output = Fragment;
+trait BetweenFragments {
+    fn add_fragment(&self, other: &Rc<RefCell<Fragment>>) -> Rc<RefCell<Fragment>>;
+    fn cmp_fragment(&self, other: &Rc<RefCell<Fragment>>) -> bool;
+}
 
-    fn add(self, other: &'b Fragment) -> Fragment {
-        Fragment {
-            start: self.start.clone(),
-            end: other.end.clone(),
-            arcs: Rc::new([&self.arcs[..], &other.arcs[..]].concat()),
-        }
+impl BetweenFragments for Rc<RefCell<Fragment>> {
+    fn add_fragment(&self, other: &Rc<RefCell<Fragment>>) -> Rc<RefCell<Fragment>> {
+        Rc::new(RefCell::new(Fragment {
+            start: self.borrow().start.clone(),
+            end: other.borrow().end.clone(),
+            arcs: [&self.borrow().arcs[..], &other.borrow().arcs[..]].concat(),
+        }))
+    }
+
+    fn cmp_fragment(&self, other: &Rc<RefCell<Fragment>>) -> bool {
+        let f = self.borrow();
+        let g = other.borrow();
+        f.start == g.start && f.end == g.end && f.arcs == g.arcs
     }
 }
 
 #[derive(Default)]
 struct Stitch {
-    stitched_arcs: HashMap<usize, i32>,
-    fragment_by_start: HashMap<Vec<i32>, Fragment>,
-    fragment_by_end: HashMap<Vec<i32>, Fragment>,
+    stitched_arcs: HashSet<usize>,
+    fragment_by_start: HashMap<[i32; 2], Rc<RefCell<Fragment>>>,
+    fragment_by_end: HashMap<[i32; 2], Rc<RefCell<Fragment>>>,
     fragments: Vec<Vec<i32>>,
 }
 
@@ -65,13 +70,34 @@ impl Stitch {
         Self::default().fragments(topology, arcs)
     }
 
-    fn replace(&mut self, fragment: &Fragment) {
+    fn replace(&mut self, fragment: Rc<RefCell<Fragment>>) {
+        let start = fragment.borrow().start.clone();
+        let end = fragment.borrow().end.clone();
         self.fragment_by_start
-            .get_mut(&fragment.start[..])
-            .map(|v| *v = fragment.clone());
+            .entry(start)
+            .and_modify(|v| *v = fragment.clone())
+            .or_insert(fragment.clone());
         self.fragment_by_end
-            .get_mut(&fragment.end[..])
-            .map(|v| *v = fragment.clone());
+            .entry(end)
+            .and_modify(|v| *v = fragment.clone())
+            .or_insert(fragment.clone());
+    }
+
+    fn replace_from(
+        &mut self,
+        fg: Rc<RefCell<Fragment>>,
+        f: Rc<RefCell<Fragment>>,
+        g: Rc<RefCell<Fragment>>,
+    ) {
+        {
+            let start = f.borrow().start.clone();
+            let end = g.borrow().end.clone();
+
+            let mut fg_borrow = fg.borrow_mut();
+            fg_borrow.start = start;
+            fg_borrow.end = end;
+        }
+        self.replace(fg);
     }
 
     fn fragments(mut self, topology: &TopoJSON, mut arcs: Vec<i32>) -> Vec<Vec<i32>> {
@@ -100,111 +126,90 @@ impl Stitch {
             arcs[j] = t;
         }
 
+        // HashMap<KeyType, Rc<RefCell<T>>>
+
         for i in arcs.iter() {
             let (start, end) = self.ends(&topology, i);
 
-            if let Some(f_end) = self
-                .fragment_by_end
-                .get(&start[..])
-                .and_then(|f| Some(f.end.clone()))
-            {
-                self.fragment_by_end.get_mut(&start[..]).map(|f| {
-                    f.push(*i);
-                    f.end = Rc::new(end);
-                });
-                self.fragment_by_end.remove(&f_end[..]);
-
-                let f = &self.fragment_by_end[&start[..]].clone();
-
-                if let Some(g_start) = self
-                    .fragment_by_start
-                    .get(&f.end[..])
-                    .and_then(|g| Some(g.start.clone()))
+            if let Some(f) = self.fragment_by_end.get(&start).cloned() {
+                self.fragment_by_end.remove(&f.borrow().end);
                 {
-                    self.fragment_by_start.remove(&g_start[..]);
-                    let g = &self.fragment_by_start[&f.end[..]].clone();
-
-                    let mut fg = if f == g { f.clone() } else { f + g };
-
-                    fg.start = f.start.clone();
-                    fg.end = g.end.clone();
-
-                    self.replace(&fg);
-                } else {
-                    self.replace(&f);
+                    f.borrow_mut().push(*i);
+                    f.borrow_mut().end = end.clone();
                 }
-            } else if let Some(f_start) = self
-                .fragment_by_start
-                .get(&end[..])
-                .and_then(|f| Some(f.start.clone()))
-            {
-                self.fragment_by_start.remove(&f_start[..]);
-                self.fragment_by_start.get_mut(&end[..]).map(|f| {
-                    f.unshift(*i);
-                    f.start = Rc::new(start);
-                });
 
-                let f = &self.fragment_by_end[&end[..]].clone();
+                if let Some(g) = self.fragment_by_start.get(&end).cloned() {
+                    self.fragment_by_start.remove(&g.borrow().start);
 
-                if let Some(g_end) = self
-                    .fragment_by_end
-                    .get(&f.start[..])
-                    .and_then(|g| Some(g.end.clone()))
-                {
-                    self.fragment_by_end.remove(&g_end[..]);
-                    let g = &self.fragment_by_end[&f.start[..]].clone();
-
-                    let mut gf = if f == g { f.clone() } else { g + f };
-
-                    gf.start = g.start.clone();
-                    gf.end = f.end.clone();
-
-                    self.replace(&gf);
+                    let fg = if f.cmp_fragment(&g) {
+                        f.clone()
+                    } else {
+                        f.add_fragment(&g)
+                    };
+                    self.replace_from(fg, f.clone(), g.clone());
                 } else {
-                    self.replace(&f);
+                    self.replace(f.clone());
+                }
+            } else if let Some(f) = self.fragment_by_start.get(&end[..]).cloned() {
+                self.fragment_by_start.remove(&f.borrow().start);
+                {
+                    f.borrow_mut().unshift(*i);
+                    f.borrow_mut().start = start.clone();
+                }
+
+                if let Some(g) = self.fragment_by_end.get(&start).cloned() {
+                    self.fragment_by_end.remove(&g.borrow().end);
+
+                    let fg = if f.cmp_fragment(&g) {
+                        f.clone()
+                    } else {
+                        g.add_fragment(&f)
+                    };
+                    self.replace_from(fg, f.clone(), g.clone());
+                } else {
+                    self.replace(f.clone());
                 }
             } else {
-                self.replace(&Fragment::new(start, end, vec![*i]));
+                self.replace(Rc::new(RefCell::new(Fragment::new(start, end, vec![*i]))));
             }
         }
 
         self.flush();
 
         arcs.iter()
-            .filter(|&&i| {
-                self.stitched_arcs
-                    .get(&arc_index(i))
-                    .map_or(true, |v| *v != 0)
-            })
+            .filter(|&&i| !self.stitched_arcs.contains(&arc_index(i)))
             .for_each(|&i| self.fragments.push(vec![i]));
         self.fragments
     }
 
-    // TODO: try to replace `Vec<i32>` by `[i32; 2]`
-    fn ends(&self, topology: &TopoJSON, &i: &i32) -> (Vec<i32>, Vec<i32>) {
+    fn ends(&self, topology: &TopoJSON, &i: &i32) -> ([i32; 2], [i32; 2]) {
         let arc = &topology.arcs[if i < 0 { !i as usize } else { i as usize }];
-        let p0 = arc.first().unwrap().to_vec();
+        let first = arc.first().unwrap();
+        let p0: [i32; 2] = std::array::from_fn(|i| first[i]);
 
-        let p1 = if topology.transform.is_some() {
-            vec![
+        let p1: [i32; 2] = if topology.transform.is_some() {
+            [
                 arc.iter().map(|x| x[0]).reduce(|a, b| a + b).unwrap(),
                 arc.iter().map(|x| x[1]).reduce(|a, b| a + b).unwrap(),
             ]
         } else {
-            arc.last().unwrap().to_vec()
+            let last = arc.last().unwrap();
+            std::array::from_fn(|i| last[i])
         };
         if i < 0 { (p1, p0) } else { (p0, p1) }
     }
 
     fn flush(&mut self) {
         let mut process_fragments =
-            |fragments: Values<Vec<i32>, Fragment>, other_map: &mut HashMap<Vec<i32>, Fragment>| {
+            |fragments: Values<[i32; 2], Rc<RefCell<Fragment>>>,
+             other_map: &mut HashMap<[i32; 2], Rc<RefCell<Fragment>>>| {
                 fragments.for_each(|f| {
-                    other_map.remove(&f.start[..]);
-                    f.arcs.iter().for_each(|&i| {
-                        self.stitched_arcs.get_mut(&arc_index(i)).map(|v| *v = 1);
+                    let start = f.borrow().start;
+                    other_map.remove(&start);
+                    f.borrow().arcs.iter().for_each(|&i| {
+                        self.stitched_arcs.insert(arc_index(i));
                     });
-                    self.fragments.push(f.arcs.to_vec());
+                    self.fragments.push(f.borrow().arcs.to_vec());
                 });
             };
         process_fragments(self.fragment_by_end.values(), &mut self.fragment_by_start);
